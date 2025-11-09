e70b843d8c21585f210eab6c65407063edb0d85d
Willem Nieuwenhuis <ruan.nieu@gmail.com>
2025-11-09 08:13:57 +0200

    Added accounts app

--- static/js/master.js
+++ static/js/master.js
@@ -1,12 +1,31 @@
 let navbar = $(".navbar-custom")
-console.log(navbar)
 
 window.addEventListener('scroll', () => {
-    console.log(window.scrollY)
     if (window.scrollY > 50) {
         navbar.addClass('hide-nav')
-        console.log(navbar)
     }else{
         navbar.removeClass('hide-nav')
     }
 })
+
+//Bar Expansion
+
+const bars = document.querySelectorAll('.bar');
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        console.log(bars)
+        if (entry.isIntersecting) {
+        // Add animation classes based on which bar it is
+            if (entry.target.classList.contains('bar1')) {
+                entry.target.classList.add('animate-left');
+            } else if (entry.target.classList.contains('bar2')) {
+                entry.target.classList.add('animate-right');
+            } else if (entry.target.classList.contains('bar3')) {
+                entry.target.classList.add('animate-left');
+            }
+            observer.unobserve(entry.target); // animate only once
+        }
+    });
+}, { threshold: 0.6 });
+
+bars.forEach(bar => observer.observe(bar));
