9542cbb8cc13b8bbbc47a284849b36d7e1dff879
Willem Nieuwenhuis <ruan.nieu@gmail.com>
2025-11-09 15:32:00 +0200

    Setting up profile view

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from django.contrib.auth import views as authviews
+from . import views
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('login/', authviews.LoginView.as_view(template_name='accounts/login.html'), name='login'),
-    path('logout/', authviews.LogoutView.as_view(), name='logout')
+    path('logout/', authviews.LogoutView.as_view(), name='logout'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('profile/<int:pk>/', views.Profile.as_view(), name='profile')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from . import forms
+from django.views.generic import CreateView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.models import User
 # Create your views here.
+
+class SignUp(CreateView):
+    form_class = forms.UserCreateForm
+    success_url = reverse_lazy('login')
+
+    template_name = 'accounts/signup.html'
+
+class Profile(LoginRequiredMixin, DetailView):
+    model = User
